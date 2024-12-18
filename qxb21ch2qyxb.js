e7a1350272d7dd0514194852952fba0a5f452168
Dyllon <dyllontan1006@gmail.com>
2024-12-18 19:51:47 +0800

    encoded

---
+++ qxb21ch2qyxb.js
@@ -0,0 +1,7 @@
+function mjxj1f2jIyx2Hyvy1Qxb21ch2qyx() {
+    const colors = ["blue", "green", "red"];
+    iyx2Hyvy1 = colors[Math.floor(Math.random() * colors.length)];
+
+    const instructions = document.getElementById("instructions");
+    instructions.innerHTML = `Avoid clicking the color: <span style="color:${iyx2Hyvy1}; font-weight:bold">${iyx2Hyvy1}</span>`;
+}
