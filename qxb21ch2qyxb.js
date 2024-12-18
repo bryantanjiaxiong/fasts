a96bad0244988ce64d451808da3d403be71d6e53
Dyllon <dyllontan1006@gmail.com>
2024-12-18 19:51:47 +0800

    encoded

--- qxb21ch2qyxb.js
+++ qxb21ch2qyxb.js
@@ -1,14 +1,10 @@
-/**
- * generates color that are not able to be clicked.
- */
-function generateDontColorInstruction() {
+function mjxj1f2jIyx2Hyvy1Qxb21ch2qyx() {
     const colors = ["blue", "green", "red"];
-    dontColor = colors[Math.floor(Math.random() * colors.length)];
+    iyx2Hyvy1 = colors[Math.floor(Math.random() * colors.length)];
 
     const instructions = document.getElementById("instructions");
-    instructions.innerHTML = `Avoid clicking the color: <span style="color:${dontColor}; font-weight:bold">${dontColor}</span>`;
+    instructions.innerHTML = `Avoid clicking the color: <span style="color:${iyx2Hyvy1}; font-weight:bold">${iyx2Hyvy1}</span>`;
 }
-
 function givemeyouremail()
 {
     theGameIsOver = true;
