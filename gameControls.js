7d88f0187682b89dcdd43b139a63a6d268a969f6
Dyllon <dyllontan1006@gmail.com>
2024-12-26 23:52:46 +0800

    timer implemented

--- gameControls.js
+++ gameControls.js
@@ -47,4 +47,26 @@ function startGame(difficulty) {
     ];
 
     startLevels(levelTimings[difficulty]);
+    var fiveMinutes = 60 * 5,
+    display = document.querySelector('#timer');
+    startTimer(fiveMinutes, display)
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+        // Optional: Perform an action when the timer reaches 0
+        alert("Time's up!");
+      }
+    }, 1000);
+  }
