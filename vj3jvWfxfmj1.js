332a08b3ce2e25d79cadffbbb296c477eb2d7a68
Dyllon <dyllontan1006@gmail.com>
2025-01-06 23:16:54 +0800

    Update vj3jvWfxfmj1.js 8%

--- vj3jvWfxfmj1.js
+++ vj3jvWfxfmj1.js
@@ -276,7 +276,7 @@ function mfwjy3j1(i)
 	for (var j = 0; j<9;j++)
 	{
 		document.getElementById("z[" + i + "][" + j + "]").style.borderColor = 'darkred';
-		document.getElementById("z[" + i + "][" + j + "]").style.borderWidth = '5%';
+		document.getElementById("z[" + i + "][" + j + "]").style.borderWidth = '8%';
 	}
 	twoojMfwjQbY3j1 = true;
 	score = document.getElementById("score");
