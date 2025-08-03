5501dd9a0285abadc50d90b2c311092066b6d549
Dyllon Tan <dyllontan1006@gmail.com>
2025-08-03 15:30:12 +1000

    Update GA.js

--- GA.js
+++ GA.js
@@ -3,9 +3,8 @@ function loadGA() {
     // Dynamically load the GA script
     var gaScript = document.createElement('script');
     gaScript.async = true;
-    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-J83EW9X1JV';
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-5YGZ8K86JH';
     document.head.appendChild(gaScript);
-
     gaScript.onload = function() {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
