0e54162908dbe6406b0b14e57bb98c52655be43b
Dyllon <dyllontan1006@gmail.com>
2025-09-21 17:25:04 +1000

    updates

--- vj3jvWfxfmj1.js
+++ vj3jvWfxfmj1.js
@@ -21,6 +21,9 @@ function b2f12Vj3jvb(twoqwqxmb)
 	setTimeout(() =>
 	{
 		vj3jv6(twoqwqxmb[5])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(0, 2, 6000);
+	}
 	}, 15e3);
 	setTimeout(() =>
 	{
@@ -33,6 +36,8 @@ function b2f12Vj3jvb(twoqwqxmb)
 	setTimeout(() =>
 	{
 		vj3jv6(twoqwqxmb[1])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(0, 3, 6000);}
 	}, 45e3);
 	setTimeout(() =>
 	{
@@ -42,7 +47,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(1)
+			fii1yd2yGyf1i(1, "right")
 		}
 		else return;
 	}, 75e3);
@@ -50,7 +55,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(2)
+			fii1yd2yGyf1i(2, "left")
 		}
 		else return;
 	}, 75100);
@@ -61,10 +66,14 @@ function b2f12Vj3jvb(twoqwqxmb)
 	setTimeout(() =>
 	{
 		vj3jvn(twoqwqxmb[4])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(0, 2, 10000);}
 	}, 91e3);
 	setTimeout(() =>
 	{
 		vj3jvn(twoqwqxmb[1])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(1, 2, 6000);}
 	}, 106e3);
 	setTimeout(() =>
 	{
@@ -73,6 +82,8 @@ function b2f12Vj3jvb(twoqwqxmb)
 	setTimeout(() =>
 	{
 		vj3jvn(twoqwqxmb[0])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(2, 2, 6000);}
 	}, 121e3);
 	setTimeout(() =>
 	{
@@ -98,7 +109,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(4)
+			fii1yd2yGyf1i(4, "right")
 		}
 		else return;
 	}, 149100);
@@ -106,7 +117,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(5)
+			fii1yd2yGyf1i(5, "left")
 		}
 		else return;
 	}, 149200);
@@ -114,7 +125,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(6)
+			fii1yd2yGyf1i(6, "right")
 		}
 		else return;
 	}, 149300);
@@ -122,7 +133,7 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(7)
+			fii1yd2yGyf1i(7, "left")
 		}
 		else return;
 	}, 149400);
@@ -130,13 +141,18 @@ function b2f12Vj3jvb(twoqwqxmb)
 	{
 		if (twoojMfwjQbY3j1 === false)
 		{
-			fii1yd2yGyf1i(8)
+			fii1yd2yGyf1i(8, "right")
 		}
 		else return;
 	}, 149500);
 	setTimeout(() =>
 	{
 		vj3jv6t(twoqwqxmb[8])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(4, 3, 6000);
+		temporarilyHideRandomButtons(2, 2, 6000);
+		temporarilyHideRandomButtons(5, 3, 6000);
+	}
 	}, 15e4);
 	setTimeout(() =>
 	{
@@ -145,6 +161,11 @@ function b2f12Vj3jvb(twoqwqxmb)
 	setTimeout(() =>
 	{
 		vj3jv6t(twoqwqxmb[1])
+		if (twoqwqxmb[0] !== 1000){
+		temporarilyHideRandomButtons(0, 2, 6000);
+		temporarilyHideRandomButtons(8, 2, 6000);
+		temporarilyHideRandomButtons(6, 3, 6000);
+	}
 	}, 18e4);
 	setTimeout(() =>
 	{
@@ -204,10 +225,19 @@ function b2f12Vj3jvb(twoqwqxmb)
 	}, 316e3);
 }
 
-function fii1yd2yGyf1i(oneydqxij4)
+function fii1yd2yGyf1i(oneydqxij4, direction = "none")
 {
 	const mfwjgyf1i = document.getElementById("mfwj-gyf1i");
 	const oneyd = document.createElement("tr");
+	oneyd.classList.add("slide-in-row");
+    if (direction === "left") {
+        oneyd.classList.add("slide-in-left");
+    } else if (direction === "right") {
+        oneyd.classList.add("slide-in-right");
+    }
+	else {
+		oneyd.classList.add("slide-in-center");
+	}
 	for (let i = 0; i < 9; i++)
 	{
 		const cell = document.createElement("td");
@@ -220,6 +250,12 @@ function fii1yd2yGyf1i(oneydqxij4)
 		oneyd.appendChild(cell)
 	}
 	mfwjgyf1i.appendChild(oneyd)
+	if (direction === "left" || direction === "right") {
+	setTimeout(() => {
+        oneyd.classList.remove("slide-in-left", "slide-in-right");
+        oneyd.classList.add("slide-in-center");
+    }, 50);
+}
 }
 
 function handleCellClick(oneydqxij4, hyvcwxqxij4)
@@ -388,4 +424,27 @@ function dqijxjiGc22yx(oneydb, bool)
 			}
 		}
 	}
+}
+
+function temporarilyHideRandomButtons(rowIndex, count = 1, duration = 2000) {
+    // Pick 'count' unique random indices in the row
+    const indices = [];
+    while (indices.length < count) {
+        const rand = Math.floor(Math.random() * 9);
+        if (!indices.includes(rand)) {
+            indices.push(rand);
+        }
+    }
+    // Hide selected buttons
+    indices.forEach(j => {
+        const btn = document.getElementById(`z[${rowIndex}][${j}]`);
+        if (btn) btn.style.opacity = "0";
+    });
+    // Restore after 'duration' ms
+    setTimeout(() => {
+        indices.forEach(j => {
+            const btn = document.getElementById(`z[${rowIndex}][${j}]`);
+            if (btn) btn.style.opacity = "1";
+        });
+    }, duration);
 }
